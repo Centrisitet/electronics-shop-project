ec3d3f251789956e50b22a9230f5bd91310c5052
centrisitet <miklekon7@gmail.com>
2023-05-17 10:19:46 +0300

    homework_1

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,19 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+
+from src.item import Item
+import pytest
+
+
+@pytest.fixture
+def item():
+    return Item('Item', 1000.0, 5)
+
+
+def test_calculate_total_price(item):
+    assert item.calculate_total_price() == 5000
+
+
+def test_apply_discount(item):
+    Item.pay_rate = 0.8
+    item.apply_discount()
+    assert item.price == 800.0
