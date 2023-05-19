39e9e21bd64b4dab01744b86694131a97802e406
centrisitet <miklekon7@gmail.com>
2023-05-19 18:33:59 +0300

    homework_1

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,10 +16,18 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.full_price = None
-        self.name = name
+        self.__name = name
         self.price = price
         self.quant = quantity
+        Item.all.append(self)
+    @property
+    def item_name(self):
+        return self.__name
+
+    @item_name.setter
+    def item_name(self, name):
+        if len(name) < 10:
+            self.__name = name
 
     def calculate_total_price(self) -> float:
         """
@@ -24,11 +35,30 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        self.full_price = self.price * self.quant
-        return self.full_price
+        full_price = self.price * self.quant
+        return full_price
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
         self.price = self.price * self.pay_rate
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('../src/items.csv', newline='') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                print(row)
+                if row[0] != 'name':
+                    name, price, quantity = row[0], row[1], row[2]
+                    cls.all.append(cls(name, price, quantity))
+
+
+
+
+    @staticmethod
+    def string_to_number(string: str):
+        num = float(string)
+        num = int(num)
+        return num
